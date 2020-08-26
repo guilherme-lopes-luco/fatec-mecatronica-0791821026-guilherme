6973e9692ea67ba59c38983d0cedb0c8bd749762
guilherme-lopes-luco <69904634+guilherme-lopes-luco@users.noreply.github.com>
2020-08-26 08:35:02 -0300

    Create programa01.c

---
+++ micro 2020/pratica02/programa01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+float a,b,x,y;
+printf("informe os valores de a,b e x;")
+scanf("%f", &a);
+scanf("%f%f%f", &a,&b, &x);
+y = (a*x) +8
+printf("resultado da equacao: %.3f/n", y);
+  return 0;
+}
