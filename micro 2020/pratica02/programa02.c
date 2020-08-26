5e0d847dafdec07f358a98029f17bb09b0938b70
guilherme-lopes-luco <69904634+guilherme-lopes-luco@users.noreply.github.com>
2020-08-26 08:54:04 -0300

    Create programa02.c

---
+++ micro 2020/pratica02/programa02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+float a,b,x;
+  printf("informe um valor para e e para a");
+  scanf("%f%f", &b, &a);
+  x = -b/a;
+  printf("valor de x: %.2f/n", x)
+  return 0;
+}
