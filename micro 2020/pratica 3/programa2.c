a0aa96da4b3e4effc2b6961dcfc14824c846cbdf
guilherme-lopes-luco <69904634+guilherme-lopes-luco@users.noreply.github.com>
2020-09-02 08:48:03 -0300

    Create programa2.c

---
+++ micro 2020/pratica 3/programa2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include 'boblioteca'
+
+int main(void) {
+  int operacao;
+  int ligado = 0;
+  do{
+    ligado = 1;
+    menu();
+    scanf("%i", &operacao);
+
+    switch(operacao){
+      case 1: realizarSoma(); break;
+      case 2: realizarSubtracao(); break;
+      case 3: realizarMultiplicacao(); break;
+      case 5: realizarSeno(); break;
+      case 4: realizarDivisao(); break;
+      case 0:
+        ligado = 0;
+        break;
+      default:
+        printf("Operacao Invalida!\n");
+        break;
+    }
+  }while(ligado != 0);
+
+  return 0;
+}
